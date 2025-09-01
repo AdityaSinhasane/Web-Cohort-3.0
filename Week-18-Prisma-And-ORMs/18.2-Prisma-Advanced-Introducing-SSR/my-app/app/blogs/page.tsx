5eba98a6071a08b58b860ed170ad741e2670855a
AdityaSinhasane <adityasinhasane100@gmail.com>
2025-09-01 13:27:26 +0530

    next-app

---
+++ Week-18-Prisma-And-ORMs/18.2-Prisma-Advanced-Introducing-SSR/my-app/app/blogs/page.tsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+async function getBlogs(){
+   const response = await axios.get("https://jsonplaceholder.typicode.com/todos/");
+   return response.data;
+}
+
+export default async function Blogs(){
+
+    const blogs = await getBlogs();
+    return (
+    <div>
+        Learn Recoil/ Redux from the best platform on the world
+        {blogs.map((blog: ITodo) => <Todo title={blog.title} completed={blog.completed}/> )}
+    </div>
+    );
+}
+
+
+interface ITodo{
+    title: string,
+    completed: boolean
+}
+
+
+function Todo({title, completed}: ITodo) {
+    return (
+        <div>
+            <h2>{title}</h2>
+            <p>{completed ? "Completed" : "Not Completed"}</p>
+        </div>
+    );
+}

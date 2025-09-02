744b95ebfab83af0f4539424f799e43310c10fb3
AdityaSinhasane <adityasinhasane100@gmail.com>
2025-09-02 19:12:03 +0530

    Prisma-Integration-with-NextJS

--- Week-19-NextJS/19.1-NextJS-Continuation-Part-2/my-app/app/signup/page.tsx
+++ Week-19-NextJS/19.1-NextJS-Continuation-Part-2/my-app/app/signup/page.tsx
@@ -1,20 +1,24 @@
 "use client"
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useRef } from "react";
 
 export default function Signup(){
     const usernameRef = useRef("");
     const passwordRef = useRef("");
+    const router = useRouter();
 
     return <div className="w-screen h-screen flex justify-center items-center">
         <div className="border p-2">
             <input type="text" placeholder="username" ref={usernameRef}/>
             <input type="password" placeholder="password" ref={passwordRef}/>
-            <button onClick={()=>{
-                axios.post("http://localhost:3000/api/v1/signup",{
+            <button onClick={async ()=>{
+                await axios.post("http://localhost:3000/api/v1/signup",{
                     username: usernameRef.current.value,
                     password: passwordRef.current.value
                 });
+                router.push("/signin");
+
             }}>Sign up</button>
         </div>
     </div>

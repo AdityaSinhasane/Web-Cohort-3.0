2cd2bb780f95568107d2f6e485fcd33998454b69
AdityaSinhasane <adityasinhasane100@gmail.com>
2025-09-06 15:32:01 +0530

    added initial chat page

---
+++ Week-21-Monorepos-And-Turborepo/21.2-Monorepos-Continued/chat-app/packages/ui/src/text-input.tsx
@@ -0,0 +1,20 @@
+interface PropType{
+    placeholder?: string,
+    size: "big" | "small",
+    onChange: (e: any)=> void
+}
+
+export function TextInput({placeholder, size, onChange}: PropType) {
+    return <input onChange={onChange} style={
+        {
+            padding: size ==="big"? "20px" : "10px", 
+            margin: size === "big"? "20px": "10px",
+            borderColor:"black", 
+            borderWidth:1, 
+            borderRadius:"4px"
+        }
+    } 
+    type="text" 
+    placeholder={placeholder} />
+
+}

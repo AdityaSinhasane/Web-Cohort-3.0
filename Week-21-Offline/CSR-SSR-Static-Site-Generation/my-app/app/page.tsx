f2f3394c1c5692f362d28e1634ed2d1f954e8670
AdityaSinhasane <adityasinhasane100@gmail.com>
2025-09-07 18:38:08 +0530

    SSR-CSR-SSG

---
+++ Week-21-Offline/CSR-SSR-Static-Site-Generation/my-app/app/page.tsx
@@ -0,0 +1,25 @@
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+export default async function Home() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data: Todo[] = await response.json();
+
+  console.log("data found from the backend server is");
+  console.log(JSON.stringify(data));
+
+  return (
+    <div>
+      {data.map((todo) => (
+        <div key={todo.id}>
+          {todo.title}
+          <br />
+          {todo.completed ? "Completed" : "Not Completed"}
+        </div>
+      ))}
+    </div>
+  );
+}
